6d3debd71101b874a1aa1033e6f6a847e564ec1d
Tanmay Bhardwaj <tanmaybharadwaj484@gmail.com>
2020-06-29 20:05:58 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
 
   render(){
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+      </BrowserRouter>
+      
     );
   }
 }
