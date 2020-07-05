aea2e742818a4387b936bbceb1d0a816c088e312
Tanmay Bhardwaj <tanmaybharadwaj484@gmail.com>
2020-07-06 01:19:34 +0530

    Assignment 4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from "react-animation-components";
 
 function RenderLeader({leader}) {
     return(
         <div key={leader.id} className="col-12 mt-5">
-            <Media tag="li">
-                <Media left middle>
-                    <Media object src={leader.image} alt={leader.name} />
+            <Fade in>
+                <Media tag="li">
+                        <Media left middle>
+                            <Media object src={baseUrl + leader.image} alt={leader.name} />
+                        </Media>
+                        <Media body className="col-12">
+                            <Media heading>{leader.name}</Media>
+                            <p>{leader.designation}</p>
+                            <p>{leader.description}</p>
+                        </Media> 
                 </Media>
-                <Media body className="col-12">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                </Media>    
-            </Media>
+            </Fade>    
         </div>
     );
 }
@@ -83,7 +87,7 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                         {leaders}
+                        <Stagger in>{leaders}</Stagger>
                     </Media>
                 </div>
             </div>
